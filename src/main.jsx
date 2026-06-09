6e2a82896dd0f18b66c65ad85c3d65482970e9e0
hotolounge-svg <lesliechoe6682@gmail.com>
2026-06-09 15:30:43 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './hotolounge.jsx'
+import App from './app1.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
